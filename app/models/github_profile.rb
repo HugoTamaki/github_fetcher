00cc857dab6d08fee8f76ef78e4d90c3c28ba8a5
Hugo Tamaki <hugotamaki@gmail.com>
2025-10-10 15:06:26 -0300

    Adding github_profiles validations

--- app/models/github_profile.rb
+++ app/models/github_profile.rb
@@ -1,2 +1,4 @@
 class GithubProfile < ApplicationRecord
+  validates :name, :github_url, :followers_count, :following_count, :stars_count, :contributions_count, :image_url, presence: true
+  validates_numericality_of :followers_count, :following_count, :stars_count, :contributions_count, only_integer: true, greater_than_or_equal_to: 0
 end
--- spec/models/github_profile_spec.rb
+++ spec/models/github_profile_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe GithubProfile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe GithubProfile, type: :model do
+  describe "validations" do
+    it "validates fields" do
+      profile = GithubProfile.new
+      expect(profile.valid?).to be_falsey
+      expect(profile.errors[:name]).to include("can't be blank")
+      expect(profile.errors[:github_url]).to include("can't be blank")
+      expect(profile.errors[:followers_count]).to include("can't be blank")
+      expect(profile.errors[:following_count]).to include("can't be blank")
+      expect(profile.errors[:stars_count]).to include("can't be blank")
+      expect(profile.errors[:contributions_count]).to include("can't be blank")
+    end
+
+    it "validates numericality" do
+      profile = GithubProfile.new(followers_count: "a", following_count: "a", stars_count: "a", contributions_count: "a")
+      expect(profile.valid?).to be_falsey
+      expect(profile.errors[:followers_count]).to include("is not a number")
+      expect(profile.errors[:following_count]).to include("is not a number")
+      expect(profile.errors[:stars_count]).to include("is not a number")
+      expect(profile.errors[:contributions_count]).to include("is not a number")
+    end
+    
+    it "saves valid data" do
+      profile = create(:github_profile)
+      expect(profile.valid?).to be_truthy
+    end
+  end
 end
